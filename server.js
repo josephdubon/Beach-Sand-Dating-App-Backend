55e2cdf0e46b55f3bf07b7eb595748c73c778e12
Joseph Dubon <josephdubon@pm.me>
2022-02-23 17:33:21 -0800

    created new server.js file, added basic express config with a home router

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+// import mongoose from "mongoose"
+
+// App Config
+const app = express()
+const port = process.env.PORT || 8001
+
+// Middleware
+
+// DB Config
+
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send('Hello Joseph Dubon'))
+
+// Listener Config
+app.listen(port, () => console.log(`Currently listening on localhost: ${port}`))
